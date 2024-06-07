f78a1a79c700ef5183f491af3e5e9027939b2ff7
Fiifi Dawson <edem.fiifi.dawson@gmail.com>
2024-06-07 05:24:26 +0000

    Fast-API
    Bug Fix

---
+++ Semantic_Search_for_YT_Videos/data-engineering/.ipynb_checkpoints/my_sk-checkpoint.py
@@ -0,0 +1,3 @@
+# YouTube key
+# TODO Get YouTube API Key
+my_key = 'AIzaSyBuSulh02KzATbNNHZFOAflkZqhpirn32E'
--- Semantic_Search_for_YT_Videos/machine_learning-engineering/app/main.py
+++ Semantic_Search_for_YT_Videos/machine_learning-engineering/app/main.py
@@ -8,13 +8,13 @@ from function import returnSearchResultIndexes
 
 # define model info 
 model_name = 'all-MiniLM-L6-v2'
-model_path = "app/data/" + model_name
+model_path = "data/" + model_name
 
 # load model
 model = SentenceTransformer(model_path)
 
 # load video index
-df = pl.scan_parquet('app/data/video-index.parquet')
+df = pl.scan_parquet('data/video-index.parquet')
 
 # create distance metric object
 dist_name = 'manhattan'
